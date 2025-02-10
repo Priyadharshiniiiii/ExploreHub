51673ac5154581a498f1e7612afbce42e9e7c0dd
Priyadharshini S <136162633+Priyadharshiniiiii@users.noreply.github.com>
2025-02-10 12:18:44 +0530

    Update useFetch.js

--- tour-management/frontend/src/hooks/useFetch.js
+++ tour-management/frontend/src/hooks/useFetch.js
@@ -9,7 +9,7 @@ const useFetch = (url) => {
         const fetchData = async() => {
             setLoading(true)
             try{
-                const res = await fetch(url)
+                const res = await fetch(`https://explorehub-backend.onrender.com`)
                 if(!res.ok){
                     setError('failed to fetch')
                 }
